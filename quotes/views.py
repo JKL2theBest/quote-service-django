734e519cd5b18d002239cb4f15d2a24df13bea52
JKL2theBest <muhammet.jkl2.suhanguylev@gmail.com>
2025-06-29 21:35:05 +0300

    Page implement

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import random
+from django.db.models import F
+from .models import Quote
 
-# Create your views here.
+def random_quote_view(request):
+    """
+    View для отображения случайной цитаты с учетом веса.
+    """
+    quotes_with_weights = list(Quote.objects.values_list('id', 'weight'))
+
+    if not quotes_with_weights:
+        return render(request, 'quotes/random_quote.html', {'quote': None})
+
+    quote_ids, weights = zip(*quotes_with_weights)
+
+    random_quote_id = random.choices(quote_ids, weights=weights, k=1)[0]
+
+    Quote.objects.filter(pk=random_quote_id).update(views=F('views') + 1)
+
+    quote = Quote.objects.get(pk=random_quote_id)
+
+    context = {
+        'quote': quote,
+    }
+    return render(request, 'quotes/random_quote.html', context)
